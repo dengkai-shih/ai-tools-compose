13cb2868e5ce47681c37c28f50623062ae24de8b
dengkaitraining <dengkai.training@gmail.com>
2026-07-16 23:28:39 +0800

    2026-07-16-v3 dcoker yaml 設定檔： 1. ollama 大型語言模型載具。 2. open webui 網頁版問答介面。 3. qdrant 向量資料庫。 4. n8n 自動化服務平台。 5. postgres 關聯式資料庫。 6. apache tika 服務平台。

--- docs/docker/bash/02-ollama-pull-module.sh
+++ docs/docker/bash/02-ollama-pull-module.sh
@@ -7,20 +7,26 @@
 #
 # 1. pull ollama LLM Modul
 
-# 1-1. "huihui_ai/gemma-4-abliterated" Effective 2B (E2B) (7.2GB)
+# [o] 1-1. "llama3.2" 3 billion Parameter Count (2.0GB)
 docker exec -it ollama ollama pull llama3.2:latest
 
-# 1-2. "huihui_ai/gemma-4-abliterated" Effective 2B (E2B) (7.2GB)
+# [o] 1-2. "huihui_ai/gemma-4-abliterated" Effective 2B (E2B) (7.2GB)
 docker exec -it ollama ollama pull huihui_ai/gemma-4-abliterated:e2b
 
-# 1-3. "huihui_ai/gllama3.2-abliterate" 3 billion Parameter Count (2.2GB)
+# [ ] 1-3. "huihui_ai/gllama3.2-abliterate" 3 billion Parameter Count (2.2GB)
 docker exec -it ollama ollama pull huihui_ai/llama3.2-abliterate:3b
 
-# 1-4. "huihui_ai/qwen3.5-abliterated" 2 billion Parameter Count (1.9GB)
+# [o] 1-4. "huihui_ai/qwen3.5-abliterated" 2 billion Parameter Count (1.9GB)
 docker exec -it ollama ollama pull huihui_ai/qwen3.5-abliterated:2B
 
-# 1-5. "translategemma model" 4 billion Parameter Count (3.3GB)
+# [o] 1-5. "huihui_ai/deepseek-r1-abliterated:1.5b" 1.5 billion Parameter Count (1.1GB)
+docker exec -it ollama ollama pull huihui_ai/deepseek-r1-abliterated:1.5b
+
+# [o] 2-1. "translategemma model" 4 billion Parameter Count (3.3GB)
 docker exec -it ollama ollama pull translategemma:4b
 
-# 1-6. mxbai-embed-large (670MB)
+# [o] 3-1. "embeddinggemma" (622MN)
+docker exec -it ollama ollama pull embeddinggemma:latest
+
+# [ ] 3-2. mxbai-embed-large (670MB)
 docker exec -it ollama ollama pull mxbai-embed-large:latest
